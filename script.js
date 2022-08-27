038fb7b1d4b5f3a9d9e9f7e248300815dfbf6556
stealthman1 <timur.marufiy@gmail.com>
2022-08-27 20:44:33 +0300

    JS - Favorites DOM Elements

--- script.js
+++ script.js
@@ -13,8 +13,10 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = [];
 let favorites = {};
 
-function updateDOM() {
-    resultsArray.forEach((result) => {
+function createDOMNodes(page) {
+    const currentArray = page === 'results' ? resultsArray : Object.values(favorites);
+    console.log('Current Array', page, currentArray);
+    currentArray.forEach((result) => {
         // Card Container
         const card = document.createElement('div');
         card.classList.add('card');
@@ -63,13 +65,21 @@ function updateDOM() {
     });
 }
 
+function updateDOM(page) {
+    // Get Favorites from localStorage
+    if (localStorage.getItem('nasaFavorites')) {
+        favorites = JSON.parse(localStorage.getItem('nasaFavorites'));
+        console.log('favorites from localStorage', favorites);
+    }
+    createDOMNodes(page);
+}
+
 // Get 10 images from NASA API
 async function getNasaPictures() {
     try {
         const response = await fetch(apiUrl);
         resultsArray = await response.json();
-        console.log(resultsArray);
-        updateDOM();
+        updateDOM('favorites');
     } catch (error) {
         // Catch Error Here
     }
